499dcb6df95c96f038e1dd6c9aaf587a4c96287d
Fineax <michal.kanarek@gmail.com>
2020-10-25 16:42:58 +0100

    Initial commit

---
+++ src/main/java/pl/kanarek/vouchershop/App.java
@@ -0,0 +1,9 @@
+package pl.kanarek.vouchershop;
+
+public class App {
+    public static void main(String[] args){
+        System.out.println("Hello word");
+    }
+}
+
+
---
+++ src/test/java/pl/kanarek/vouchershop/catalog/ProductCatalogTest.java
@@ -0,0 +1,16 @@
+package pl.kanarek.vouchershop.catalog;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class ProductCatalogTest {
+    @Test
+    public void itAllowToRegisterNewProduct(){
+        ProductCatalog catalog = therIsProductCatalog();
+
+        string productId = catalog.registerProduct();
+
+        Assert.assertTrue(catalog.isExist(productId));
+    }
+
+}
