692e303700b00ee727d3de23cf02a890cd0cb4bf
Michal <michal.kanarek@gmail.com>
2021-01-11 09:36:56 +0100

    Add jdbc extenstions

---
+++ src/test/java/pl/kanarek/vouchershop/catalog/jdbcProductStorageTest.java
@@ -0,0 +1,20 @@
+package pl.kanarek.vouchershop.catalog;
+
+import org.junit.Test;
+
+public class jdbcProductStorageTest {
+
+
+    @Test
+    public void itAllowAddProduct(){
+
+    }
+    @Test
+    public void itAllowLoadAllProducts(){
+
+    }
+    @Test
+    public void itAllowCheckIfProductExists(){
+
+    }
+}
